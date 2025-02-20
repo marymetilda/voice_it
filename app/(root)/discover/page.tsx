46888540d02dcf0b68b7efc76599e915fc5e37b1
marymetilda <metildaphy@gmail.com>
2025-02-20 11:16:08 +0530

    base layout structure

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className="text-20 font-bold text-white-1">Discover</h1>
+    </div>
+  );
 };
 
 export default page;
